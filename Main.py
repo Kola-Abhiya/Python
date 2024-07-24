b0797b71b9f125396ed5e5bec7ce984175a727eb
Kola-Abhiya <127921493+Kola-Abhiya@users.noreply.github.com>
2024-07-24 15:12:43 +0530

    Create Main.py
    This is the first repo dude

---
+++ Main.py
@@ -0,0 +1,13 @@
+import datetime
+
+# Get the current date and time
+now = datetime.datetime.now()
+
+# Print the current date and time
+print("Current date and time:", now)
+
+# Print the current date
+print("Current date:", now.date())
+
+# Print the current time
+print("Current time:", now.time())
